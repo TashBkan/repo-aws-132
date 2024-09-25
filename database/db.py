01659f907d673f56032c96b95620816b0270a802
EC2 Default User <ec2-user@ip-172-31-20-248.us-west-1.compute.internal>
2024-09-25 03:54:40 +0000

    version1

---
+++ database/db.py
@@ -0,0 +1,27 @@
+import pymysql
+
+endpoint = 'database-132.c5i4oqkack3f.us-west-1.rds.amazonaws.com'
+user = 'alejo'
+passw = '321654+-'
+
+def connectionSQL():
+    try:
+        obj_connect = pymysql.connect(
+            host=endpoint,
+            user=user,
+            password=passw
+        )
+        print("Succesfull connection to a database")
+        return obj_connect
+    except Exception as err:
+        print("Error:", err)
+        obj_connect = None
+
+def add_user(id, name, lastname, birtthday):
+    try:
+        instruction_sql = "INSERT INTO db_users.user (id, name, lastname, birthday) VALUES("+id+", '"+name+"','"+lastname+"','"+birtthday+"')"
+        object_connect = connectionSQL()
+        object_connect.cursor().execute(instruction_sql).commit()
+        print("The user was added")
+    except Exception as err:
+        print("Error:", err)
---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from database.db import connectionSQL, add_user
+
+app = Flask(__name__)
+
+@app.route('/register_page')
+def register():
+    return render_template("register.html")
+
+@app.route('/register_user', methods=["post"])
+def register_user():
+    data_user = request.form
+    id = data_user["id"]
+    name = data_user["naem"]
+    lastname = data_user["lastname"]
+    birthday = data_user["birthday"]
+    add_user(id, name, lastname, birthday)
+    return "User added"
+
+if __name__ == "__main__":
+    ip = "172.31.20.248"
+    port = "80"
+    app.run(ip, port)
